7c5ddf471d6bcf314e7a84d63d23f308d204d9d4
LopezJuanAndres <andres.juan.lopez@gmail.com>
2025-03-27 20:45:42 -0300

    dockerizando proyecto

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -5,7 +5,7 @@ class Conexion extends PDO {
 public function __construct(){
 
     try{
-        parent::__construct('mysql:host=localhost; dbname=gestorasistencia','root','admin123');
+        parent::__construct('mysql:host=mysql_db:3306; dbname=gestorasistencia','root','root');
         parent::exec("set names utf8");
     }catch(PDOException $e){
         echo "Error al conectar". $e->getMessage();
